56c21db20289a87e24664e4255df4fff158c188f
AndriyShkrobut <andyshkrobut0107@gmail.com>
2019-03-06 00:24:57 +0200

    Added Product Model and Products Migration, Controller, View

---
+++ EpamProject/WebShop/Data/WebShopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShop.Models
+{
+    public class WebShopContext : DbContext
+    {
+        public WebShopContext (DbContextOptions<WebShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebShop.Models.Product> Product { get; set; }
+    }
+}
