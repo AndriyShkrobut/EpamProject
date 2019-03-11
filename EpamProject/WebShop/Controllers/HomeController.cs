c2dcbdd1301dd9bbe378c9e125bee8759535b4f0
AndriyShkrobut <andyshkrobut0107@gmail.com>
2019-03-11 09:58:13 +0200

    Switched .NET from 2.1 to 2.2

--- EpamProject/WebShop/Controllers/HomeController.cs
+++ EpamProject/WebShop/Controllers/HomeController.cs
@@ -8,17 +8,22 @@ using WebShop.Models;
 
 namespace WebShop.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      return View();
     }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
